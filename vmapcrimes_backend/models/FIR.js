33d4b58e57e0bd5f0457df9604043286db81ba7e
Abhinav Shukla <abhinavshukla@pop-os.localdomain>
2023-02-08 14:30:29 +0530

    Added route for upload, model for FIR and modified the main.js file

---
+++ vmapcrimes_backend/models/FIR.js
@@ -0,0 +1,69 @@
+const mongoose = require('mongoose');
+const {Schema} = mongoose;
+const FIRSchema = new Schema({
+    FIR_Number:{
+        type: Number,
+        require: true
+    },
+    Date:{
+        type: Date,
+        default: Date.now
+    },
+    Police_Station_ID:{
+        type: Number,
+        require: true
+    },
+    Created_At:{
+        type: String,
+        require: true
+    },
+    Name:{
+        type: String,
+        require: true
+    },
+    Parent_Name:{
+        type: String,
+        require: true
+    },
+    Address:{
+        type: String,
+        require: true
+    },
+    Zip:{
+        type: Number,
+        require: true
+    },
+    Contact_Number:{
+        type: Number,
+        require: true
+    },
+    Relation_with_accused:{
+        type: String,
+        require: true
+    },
+    Name_of_accused:{
+        type: String,
+        require: true
+    },
+    Type_of_incident:{
+        type: String,
+        require: true
+    },
+    Incident_details:{
+        type: String,
+        require: true
+    },
+    Location: {
+        type: {
+            type: String,
+            enum: ['Point'],
+            required: true
+        },
+        coordinates: {
+            type: [Number],
+            required: true
+        }
+    }
+});
+FIRSchema.index({ Location: '2dsphere' });
+module.exports = mongoose.model('FIR', FIRSchema)
